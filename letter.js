ea2cd554d2b413f653dfb9f8e4344cbafd8cddb2
teefeid <taef_415@hotmail.com>
2021-08-03 11:46:02 +0300

    Warm Up

---
+++ letter.js
@@ -0,0 +1,19 @@
+var letters = {
+    A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+    D: 2, G: 2,
+    B: 3, C: 3, M: 3, P: 3,
+    F: 4, H: 4, V: 4, W: 4, Y: 4,
+    K: 5,
+    J: 8, X: 8,
+    Q: 10, Z: 10
+};
+var scrabble = function (name) {
+    var value = 0;
+    name = name.toUpperCase();
+    for (var i = 0; i < name.length; i++) {
+        console.log(name[i]);
+        value += letters[name[i]];
+    }
+    return value;
+};
+console.log(scrabble("taif"));
