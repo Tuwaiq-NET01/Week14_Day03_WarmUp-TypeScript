4c54c0217be64f3452edf5f43d91e7f6f7f09967
Nada Al-Otaibi <82446110+NadaAlOtaibi@users.noreply.github.com>
2021-08-03 11:54:40 +0300

    Update Alpha.js

--- Apha/Alpha.js
+++ Apha/Alpha.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.__esModule = true;
-var weights = {
+var Alpha = {
     A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
     D: 2, G: 2,
     B: 3, C: 3, M: 3, P: 3,
@@ -9,13 +9,13 @@ var weights = {
     J: 8, X: 8,
     Q: 10, Z: 10
 };
-function score(word) {
+function score(string) {
     if (!word) {
         return 0;
     }
     var res = 0;
-    for (var i = 0; i < word.length; i++) {
-        res += weights[word[i].toUpperCase()];
+    for (var i = 0; i < string.length; i++) {
+        res += Alpha[string[i].toUpperCase()];
     }
     return res;
 }
