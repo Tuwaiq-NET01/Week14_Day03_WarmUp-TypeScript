2692691818524dba74fbeee5de191bce37fdb574
Taghreed <Taghreed.Alqahtani@tuwaiq.edu.sa>
2021-08-04 05:41:02 +0300

    typescript warm up

---
+++ typescript_warmUp/Scrabble.js
@@ -0,0 +1,23 @@
+"use strict";
+exports.__esModule = true;
+var Score = {
+    A: 1, E: 1, I: 1, O: 1, U: 1, L: 1, N: 1, R: 1, S: 1, T: 1,
+    D: 2, G: 2,
+    B: 3, C: 3, M: 3, P: 3,
+    F: 4, H: 4, V: 4, W: 4, Y: 4,
+    K: 5,
+    J: 8, X: 8,
+    Q: 10, Z: 10
+};
+function score(string) {
+    if (!string) {
+        return 0;
+    }
+    var result = 0;
+    for (var i = 0; i < string.length; i++) {
+        result += Score[string[i].toUpperCase()];
+    }
+    console.log(score("hello World"));
+    return result;
+}
+exports["default"] = score;
